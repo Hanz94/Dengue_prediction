18a6f4bb7f94600e674924ecb4b13059b9d84ed5
Hanz94 <hansikam94@gmail.com>
2017-05-21 18:14:04 +0530

    Cross corelation completed

--- cc_calculation.R
+++ cc_calculation.R
@@ -11,7 +11,8 @@ Find_Max_CCF<- function(a,b)
   cor = d$acf[,,1] 
   lag = d$lag[,,1] 
   res = data.frame(cor,lag) 
-  res_max = res[which.max(abs(res$cor)),] 
+  res <- filter(res, lag >= 0)         #consider only lags not leads
+  res_max = res[which.max(res$cor),] 
   return(res_max) 
 }
 
@@ -29,7 +30,7 @@ Get_Best_lag<- function(city,attri)
   else
   {
     for (attr in attri) {
-      Find_Max_CCF(df_iq["total_cases"], df_iq[attr])
+      temp <- Find_Max_CCF(df_iq["total_cases"], df_iq[attr])
       acf<- append(acf,temp$cor)
       lag<- append(lag,temp$lag)
     }
@@ -45,13 +46,37 @@ df_iq <- filter(combined, city == 'iq')
 
 #use this for ccf visualization
 ccf(df_sj$total_cases, df_sj$reanalysis_specific_humidity_g_per_kg, lag.max=8, plot=TRUE, main="tt", na.action = na.contiguous)
+ccf(df_iq$total_cases, df_iq$reanalysis_specific_humidity_g_per_kg,lag.max=8, plot=TRUE, main="tt", na.action = na.contiguous)
+
 
 #print(Find_Max_CCF(df_sj["total_cases"], df_sj$reanalysis_specific_humidity_g_per_kg))
 
-sj_cc_all <- Get_Best_lag("sj",c("reanalysis_specific_humidity_g_per_kg" , "reanalysis_dew_point_temp_k" , "station_avg_temp_c" ,"reanalysis_max_air_temp_k"))
+sj_cc_all <- Get_Best_lag("sj",c("reanalysis_specific_humidity_g_per_kg",
+                                 "reanalysis_dew_point_temp_k",
+                                 "station_avg_temp_c",
+                                 "station_min_temp_c",
+                                 "reanalysis_min_air_temp_k",
+                                 "station_max_temp_c",
+                                 "reanalysis_max_air_temp_k"))
+
+sj_cc_all <- sj_cc_all[order(sj_cc_all$acf),]
 print(sj_cc_all)
 
-ggplot(data=sj_cc_all, aes(x=attri, y=acf, fill=acf)) +
-  geom_bar(colour="black", stat="identity")
+ggplot(data=sj_cc_all, aes(x=attri, y=acf, fill=lag)) +
+  geom_bar(colour="black", stat="identity", position="dodge")
 
+
+iq_cc_all <- Get_Best_lag("iq",c("reanalysis_specific_humidity_g_per_kg",
+                                 "reanalysis_dew_point_temp_k",
+                                 "station_avg_temp_c",
+                                 "station_min_temp_c",
+                                 "reanalysis_min_air_temp_k",
+                                 "station_max_temp_c",
+                                 "reanalysis_max_air_temp_k"))
+
+iq_cc_all <- iq_cc_all[order(iq_cc_all$acf),]
+print(iq_cc_all)
+
+ggplot(data=iq_cc_all, aes(x=attri, y=acf, fill=lag)) +
+  geom_bar(colour="black", stat="identity", position="dodge")
 
